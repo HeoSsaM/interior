539f34a1f0720f2be8ad2f52fbbdbc0c9e74deef
허혜정 <blessme.5004@gmail.com>
2025-02-11 16:34:36 +0900

    반응형 웹 페이지 interiro

---
+++ js/script.js
@@ -0,0 +1,37 @@
+$(function () {
+    //gnb
+    $('.gnb > li').on('mouseover', function () {
+        $(this).find('.subMenu').stop().slideDown();
+    });
+    $('.gnb > li').on('mouseleave', function () {
+        $(this).find('.subMenu').stop().slideUp();
+    });
+
+
+    //swiper-living_banner
+    var swiper = new Swiper(".living_banner", {
+        slidesPerView: 2,
+        spaceBetween: 20,
+        freeMode: true,
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        breakpoints: {
+            768: {
+                slidesPerView: 3,
+                spaceBetween: 20,
+            },
+            1200: {
+                slidesPerView: 4,
+                spaceBetween: 20,
+            }
+        }
+    });
+    //swiper-mobile menu
+    var swiper = new Swiper(".gnb_area.mobile", {
+        slidesPerView: 5,
+        spaceBetween: 20,
+        freeMode: true,
+    });
+})
